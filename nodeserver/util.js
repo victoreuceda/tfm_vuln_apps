cb6eea49c54fb11272dc83e04beb8dccd66aa4b1
Victor Euceda <victoreuceda@Victors-Air.home>
2024-08-24 15:15:10 +0200

    Base app v2

--- nodeserver/util.js
+++
@@ -1,7 +0,0 @@
-const marked = require('marked');
-
-function parseMarkdown(markdownText) {
-  return marked(markdownText);
-}
-
-module.exports = { parseMarkdown };
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  reflectInput(input: string): string {
-    return `Input was: ${input}`;
+  getHello(): string {
+    return 'Hello Secure World!';
   }
 }

a92bef796e10ec60913d56227e39eb56270f0f02
Victor Euceda <victoreuceda@Victors-Air.home>
2024-08-26 22:34:38 +0200

    New endpoints

--- src/users/user.dto.ts
+++ src/users/user.dto.ts
@@ -1,5 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export default class UserDto {
-    id: string;
-    username: string;
-    password: string;
+  id: string;
+  @ApiProperty({ description: 'The username of the User' })
+  username: string;
+  @ApiProperty({ description: 'The password of the User' })
+  password: string;
 }
